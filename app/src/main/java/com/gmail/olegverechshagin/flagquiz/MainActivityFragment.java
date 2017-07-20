f71cc02ab6de39e5c5d5550564df6aca394a78d5
Oleg Vereshagin <oleg.verechshagin@bas.kz>
2017-07-20 09:46:35 +0600

    Add content for tablet min 700dp.

--- app/src/main/java/com/gmail/olegverechshagin/flagquiz/MainActivityFragment.java
+++ app/src/main/java/com/gmail/olegverechshagin/flagquiz/MainActivityFragment.java
@@ -1,8 +1,23 @@
 package com.gmail.olegverechshagin.flagquiz;
 
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 /**
  * Created by oleg.verechshagin on 18.07.2017.
  */
 
-public class MainActivityFragment {
+public class MainActivityFragment extends Fragment {
+
+    public SettingsActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_settings, container, false);
+    }
 }
