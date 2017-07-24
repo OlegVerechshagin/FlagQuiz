c8955a0a79e663abbdb113938eeb263e6fa170be
Oleg Verechshagin <oleg.verechshagin@bas.kz>
2017-07-24 21:20:40 +0600

    Created method onStart.

--- app/src/main/java/com/olegverechshagin/flagquiz/MainActivity.java
+++ app/src/main/java/com/olegverechshagin/flagquiz/MainActivity.java
@@ -53,22 +53,27 @@ public class MainActivity extends AppCompatActivity {
 
 //      На телефоне разрешена только портретная ориентация
       if (phoneDevice)
-         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+         setRequestedOrientation(
+                 ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+   }
+
+//   Вызывается после завершения выполнения onCreate
+   @Override
+   protected void onStart() {
+      super.onStart();
+
+      if (preferencesChanged) {
+//         После задания настроек по умолчанию инициализировать
+//         MainActivityFragment и запустить викторину
+         MainActivityFragment quizFragment = (MainActivityFragment)
+                 getSupportFragmentManager().findFragmentById(
+                         R.id.quizFragment);
+         quizFragment.updateGuessRows(
+                 PreferenceManager.getDefaultSharedPreferences(this));
+         quizFragment.updateRegions(
+                 PreferenceManager.getDefaultSharedPreferences(this));
+         quizFragment.resetQuiz();
+         preferencesChanged = false;
+      }
    }
 }
-
-
-/*************************************************************************
- * (C) Copyright 1992-2016 by Deitel & Associates, Inc. and               *
- * Pearson Education, Inc. All Rights Reserved.                           *
- *                                                                        *
- * DISCLAIMER: The authors and publisher of this book have used their     *
- * best efforts in preparing the book. These efforts include the          *
- * development, research, and testing of the theories and programs        *
- * to determine their effectiveness. The authors and publisher make       *
- * no warranty of any kind, expressed or implied, with regard to these    *
- * programs or to the documentation contained in these books. The authors *
- * and publisher shall not be liable in any event for incidental or       *
- * consequential damages in connection with, or arising out of, the       *
- * furnishing, performance, or use of these programs.                     *
- *************************************************************************/
