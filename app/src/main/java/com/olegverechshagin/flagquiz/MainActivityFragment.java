e63c7ce1202bd1ac85f7d6d547386a276afa2dd1
Oleg Vereshagin <oleg.verechshagin@bas.kz>
2017-07-26 09:28:23 +0600

    Add imports and declaration of fields in MainActivityFragment.

--- app/src/main/java/com/olegverechshagin/flagquiz/MainActivityFragment.java
+++ app/src/main/java/com/olegverechshagin/flagquiz/MainActivityFragment.java
@@ -1,14 +1,63 @@
 // MainActivityFragment.java
-// Contains the Flag Quiz logic
+// Класс содержит логику приложения Flag Quiz
 package com.olegverechshagin.flagquiz;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.content.res.AssetManager;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
+import android.os.Handler;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewAnimationUtils;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivityFragment extends Fragment {
+//   Строка, используемая при регистрации сообщений об ошибках
+   private static final String TAG = "FlagQuiz Activity";
+
+   private static final int FLAGS_IN_QUIZ = 10;
+
+   private List<String> fileNameList; // имена файлов с флагами
+   private List<String> quizCountriesList; // страны текущей викторины
+   private Set<String> regionsSet; // регионы текущей викторины
+   private String correctAnswer; // правильная страна для текущего флага
+   private int totalGuesses; // количество попыток
+   private int correctAnswers; // количество правильных ответов
+   private int guessRows; // количество строк с кнопками вариантов
+   private SecureRandom random; // генератор случайных чисел
+   private Handler handler; // для задержки загрузки следующего флага
+   private Animation shakeAnimation; // анимация неправильного ответа
+
+   private LinearLayout quizLinearLayout; // макет с викториной
+   private TextView questionNumberTextView; // номер текущего вопроса
+   private ImageView flagImageView; // для вывода флага
+   private LinearLayout[] guessLinearLayouts; // строки с кнопками
+   private TextView answerTextView; // для привильного ответа
+
    public MainActivityFragment() {
    }
 
@@ -18,19 +67,3 @@ public class MainActivityFragment extends Fragment {
       return inflater.inflate(R.layout.fragment_settings, container, false);
    }
 }
-
-
-/*************************************************************************
- * (C) Copyright 1992-2016 by Deitel & Associates, Inc. and               *
- * Pearson Education, Inc. All Rights Reserved.                           *
- *                                                                        *
- * DISCLAIMER: The authors and publisher of this book have used their     *
- * best efforts in preparing the book. These efforts include the          *
- * development, research, and testing of the theories and programs        *
- * to determine their effectiveness. The authors and publisher make       *
- * no warranty of any kind, expressed or implied, with regard to these    *
- * programs or to the documentation contained in these books. The authors *
- * and publisher shall not be liable in any event for incidental or       *
- * consequential damages in connection with, or arising out of, the       *
- * furnishing, performance, or use of these programs.                     *
- *************************************************************************/
