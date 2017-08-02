dceaeb1ac42cbee897cbc671db2a6eefbd02cd4b
Oleg Vereshagin <oleg.verechshagin@bas.kz>
2017-08-02 09:28:57 +0600

    Create method: resetQuiz.

--- app/src/main/java/com/olegverechshagin/flagquiz/MainActivityFragment.java
+++ app/src/main/java/com/olegverechshagin/flagquiz/MainActivityFragment.java
@@ -36,78 +36,78 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainActivityFragment extends Fragment {
-//   Строка, используемая при регистрации сообщений об ошибках
-   private static final String TAG = "FlagQuiz Activity";
+    //   Строка, используемая при регистрации сообщений об ошибках
+    private static final String TAG = "FlagQuiz Activity";
 
-   private static final int FLAGS_IN_QUIZ = 10;
+    private static final int FLAGS_IN_QUIZ = 10;
 
-   private List<String> fileNameList; // имена файлов с флагами
-   private List<String> quizCountriesList; // страны текущей викторины
-   private Set<String> regionsSet; // регионы текущей викторины
-   private String correctAnswer; // правильная страна для текущего флага
-   private int totalGuesses; // количество попыток
-   private int correctAnswers; // количество правильных ответов
-   private int guessRows; // количество строк с кнопками вариантов
-   private SecureRandom random; // генератор случайных чисел
-   private Handler handler; // для задержки загрузки следующего флага
-   private Animation shakeAnimation; // анимация неправильного ответа
+    private List<String> fileNameList; // имена файлов с флагами
+    private List<String> quizCountriesList; // страны текущей викторины
+    private Set<String> regionsSet; // регионы текущей викторины
+    private String correctAnswer; // правильная страна для текущего флага
+    private int totalGuesses; // количество попыток
+    private int correctAnswers; // количество правильных ответов
+    private int guessRows; // количество строк с кнопками вариантов
+    private SecureRandom random; // генератор случайных чисел
+    private Handler handler; // для задержки загрузки следующего флага
+    private Animation shakeAnimation; // анимация неправильного ответа
 
-   private LinearLayout quizLinearLayout; // макет с викториной
-   private TextView questionNumberTextView; // номер текущего вопроса
-   private ImageView flagImageView; // для вывода флага
-   private LinearLayout[] guessLinearLayouts; // строки с кнопками
-   private TextView answerTextView; // для привильного ответа
+    private LinearLayout quizLinearLayout; // макет с викториной
+    private TextView questionNumberTextView; // номер текущего вопроса
+    private ImageView flagImageView; // для вывода флага
+    private LinearLayout[] guessLinearLayouts; // строки с кнопками
+    private TextView answerTextView; // для привильного ответа
 
-//   Настройка MainActivityFragment при создании представления
-   @Override
-   public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                            Bundle savedInstanceState) {
-      super.onCreateView(inflater, container, savedInstanceState);
-      View view =
-              inflater.inflate(R.layout.fragment_main, container, false);
+    //   Настройка MainActivityFragment при создании представления
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        View view =
+                inflater.inflate(R.layout.fragment_main, container, false);
 
-      fileNameList = new ArrayList<>();
-      quizCountriesList = new ArrayList<>();
-      random = new SecureRandom();
-      handler = new Handler();
+        fileNameList = new ArrayList<>();
+        quizCountriesList = new ArrayList<>();
+        random = new SecureRandom();
+        handler = new Handler();
 
 //      Загрузка анимации для неправильных ответов
-      shakeAnimation = AnimationUtils.loadAnimation(getActivity(),
-              R.anim.incorrect_shake);
-      shakeAnimation.setRepeatCount(3); // анимация повторяется 3 раза
+        shakeAnimation = AnimationUtils.loadAnimation(getActivity(),
+                R.anim.incorrect_shake);
+        shakeAnimation.setRepeatCount(3); // анимация повторяется 3 раза
 
 //      Получение ссылок на компоненты графического интерфейса
-      quizLinearLayout =
-              (LinearLayout) view.findViewById(R.id.quizLinearLayout);
-      questionNumberTextView =
-              (TextView) view.findViewById(R.id.questionNumberTextView);
-      flagImageView = (ImageView) view.findViewById(R.id.flagImageView);
-      guessLinearLayouts = new LinearLayout[4];
-      guessLinearLayouts[0] =
-              (LinearLayout) view.findViewById(R.id.row1LinearLayout);
-      guessLinearLayouts[1] =
-              (LinearLayout) view.findViewById(R.id.row2LinearLayout);
-      guessLinearLayouts[2] =
-              (LinearLayout) view.findViewById(R.id.row3LinearLayout);
-      guessLinearLayouts[3] =
-              (LinearLayout) view.findViewById(R.id.row4LinearLayout);
-      answerTextView = (TextView) view.findViewById(R.id.answerTextView);
+        quizLinearLayout =
+                (LinearLayout) view.findViewById(R.id.quizLinearLayout);
+        questionNumberTextView =
+                (TextView) view.findViewById(R.id.questionNumberTextView);
+        flagImageView = (ImageView) view.findViewById(R.id.flagImageView);
+        guessLinearLayouts = new LinearLayout[4];
+        guessLinearLayouts[0] =
+                (LinearLayout) view.findViewById(R.id.row1LinearLayout);
+        guessLinearLayouts[1] =
+                (LinearLayout) view.findViewById(R.id.row2LinearLayout);
+        guessLinearLayouts[2] =
+                (LinearLayout) view.findViewById(R.id.row3LinearLayout);
+        guessLinearLayouts[3] =
+                (LinearLayout) view.findViewById(R.id.row4LinearLayout);
+        answerTextView = (TextView) view.findViewById(R.id.answerTextView);
 
 //      Настройка слушателей для кнопок ответов
-      for (LinearLayout row: guessLinearLayouts) {
-         for (int column = 0; column < row.getChildCount(); column++) {
-            Button button = (Button) row.getChildAt(column);
-            button.setOnClickListener(guessButtonListener);
-         }
-      }
+        for (LinearLayout row : guessLinearLayouts) {
+            for (int column = 0; column < row.getChildCount(); column++) {
+                Button button = (Button) row.getChildAt(column);
+                button.setOnClickListener(guessButtonListener);
+            }
+        }
 
 //      Назначение текста questionNumberTextView
-      questionNumberTextView.setText(
-              getString(R.string.question, 1, FLAGS_IN_QUIZ));
-      return view; // возвращает представление фрагмента для вывода
-   }
+        questionNumberTextView.setText(
+                getString(R.string.question, 1, FLAGS_IN_QUIZ));
+        return view; // возвращает представление фрагмента для вывода
+    }
 
-//   Обновление guessRows на основании значения SharedPreferences
+    //   Обновление guessRows на основании значения SharedPreferences
     public void updateGuessRows(SharedPreferences sharedPreferences) {
 //        Получение количества отображаемых вариантов ответа
         String choices =
@@ -115,7 +115,7 @@ public class MainActivityFragment extends Fragment {
         guessRows = Integer.parseInt(choices) / 2;
 
 //        Все компоненты LinearLayout скрываются
-        for (LinearLayout layout: guessLinearLayouts)
+        for (LinearLayout layout : guessLinearLayouts)
             layout.setVisibility(View.GONE);
 
 //        Отображение нужных компонентов LinearLayout
@@ -123,9 +123,53 @@ public class MainActivityFragment extends Fragment {
             guessLinearLayouts[row].setVisibility(View.VISIBLE);
     }
 
-//    Обновление выбранных регионов по данным из SharedPreferences
+    //    Обновление выбранных регионов по данным из SharedPreferences
     public void updateRegions(SharedPreferences sharedPreferences) {
         regionsSet =
                 sharedPreferences.getStringSet(MainActivity.REGION, null);
+    }
+
+    //    Настройка и запуск следующей серии вопросов
+    public void resetQuiz() {
+//        Использование AssetManager для получения имен файлов изображений
+        AssetManager assets = getActivity().getAssets();
+        fileNameList.clear(); // пустой список имен файлов изображений
+
+        try {
+//            Перебрать все регионы
+            for (String region : regionsSet) {
+//                get a list of all flag image files in this region
+                String[] paths = assets.list(region);
+
+                for (String path : paths) {
+                    fileNameList.add(path.replace(".png", ""));
+                }
+            }
+        } catch (IOException exception) {
+            Log.e(TAG, "Error loading image file names", exception);
+        }
+
+        correctAnswers = 0; // сброс количества правильных ответов
+        totalGuesses = 0; // сброс общего количества попыток
+        quizCountriesList.clear(); // очистка предыдущего списка стран
+
+        int  flagCounter = 1;
+        int numberOfFlags = fileNameList.size();
+
+//        Добавление FLAGS_IN_QUIZ сдучайных фалов в quizCountriesList
+        while (flagCounter <= FLAGS_IN_QUIZ) {
+            int randomIndex = random.nextInt(numberOfFlags);
+
+//            Получение случайного имени файла
+            String filename = fileNameList.get(randomIndex);
+
+//            Если регион включен, но еще не был выбран
+            if (!quizCountriesList.contains(filename)) {
+                quizCountriesList.add(filename); // добавить файл в список
+                ++flagCounter;
+            }
+        }
+
+        loadNextFlag(); // запустить викторину загрузкой первого флага
     }
 }
